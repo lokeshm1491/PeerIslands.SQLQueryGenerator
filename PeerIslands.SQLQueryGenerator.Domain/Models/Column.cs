e5ab9046b37cb66c19ae71278255d39ac0657a23
Lokhes <lokhes@info-tech.com.sg>
2021-10-13 17:25:17 +0800

    Model Classes added

---
+++ PeerIslands.SQLQueryGenerator.Domain/Models/Column.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeerIslands.SQLQueryGenerator.Domain.Models
+{
+    [Serializable]
+    public class Column
+    {
+        public string Operator { get; set; }
+        public string FieldName { get; set; }
+        public string FieldValue { get; set; }
+    }
+}
---
+++ PeerIslands.SQLQueryGenerator.Domain/Models/Table.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PeerIslands.SQLQueryGenerator.Domain.Models
+{
+    [Serializable]
+    public class Table
+    {
+        public string TableName { get; set; }
+        public List<Column> Columns { get; set; }
+    }
+}
