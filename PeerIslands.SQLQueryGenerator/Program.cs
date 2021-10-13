6a25e71b67bdb2abf57f09d526cc1221de3deeee
Lokhes <lokhes@info-tech.com.sg>
2021-10-13 16:31:17 +0800

    Add project files.

---
+++ PeerIslands.SQLQueryGenerator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeerIslands.SQLQueryGenerator
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
